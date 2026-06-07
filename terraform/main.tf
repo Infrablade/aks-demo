05162d7be3762b18147f7c637dd5634189aab82a
Chris Pappas <chris.pappas@infrablade.com>
2026-06-07 18:48:18 +0100

    feat: adding ACR

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,3 +31,24 @@ resource "azurerm_kubernetes_cluster" "aks" {
     project = "aks-demo"
   }
 }
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  sku                 = "Basic"
+  admin_enabled       = false
+
+  tags = {
+    env     = "demo"
+    team    = "platform"
+    project = "aks-demo"
+  }
+}
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}

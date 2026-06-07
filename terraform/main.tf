73441a0320d8bc1e7fb9677c704ef89442713323
Chris Pappas <chris.pappas@infrablade.com>
2026-06-07 18:17:27 +0100

    feat: add AKS cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,26 @@ resource "azurerm_resource_group" "main" {
     project = "aks-demo"
   }
 }
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = var.aks_name
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = var.node_vm_size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    env     = "demo"
+    team    = "platform"
+    project = "aks-demo"
+  }
+}
